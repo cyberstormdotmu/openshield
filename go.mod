3af0537b91df3b67b72d8d039e3deb39a840bec0
poshan-p <poshanpeeroo@gmail.com>
2025-07-20 12:00:51 +0400

    feat(tls): Add hybrid post-quantum key exchange and update Go
    Updated Go to 1.24.5 and implemented hybrid post-quantum key exchange
    (X25519MLKEM768) in TLS CurvePreferences for enhanced future-proof security.
    Added `CHACHA20_POLY1305` and `AES_256_GCM` in cipher suites.
    
    Signed-off-by: Poshan Peeroo <poshanpeeroo@cyberstorm.mu>
    Signed-off-by: Kushan Gangaram <kushangangaram@cyberstorm.mu>
    Signed-off-by: Nakul Kumar Singh Baboolall <nkb@cyberstorm.mu>
    Signed-off-by: Sehun Bissessur <sehun.bissessur@cyberstorm.mu>

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/openshieldai/openshield
 
-go 1.23.0
+go 1.24.5
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
--- lib/cache.go
+++ lib/cache.go
@@ -29,8 +29,10 @@ func InitRedisClient(config *Configuration) {
         if config.Settings.Redis.SSL {
             redisTlsCfg = &tls.Config{
                 MinVersion:       tls.VersionTLS12,
-                CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+                CurvePreferences: []tls.CurveID{tls.X25519MLKEM768, tls.CurveP521, tls.CurveP384, tls.CurveP256},
                 CipherSuites: []uint16{
+					tls.TLS_CHACHA20_POLY1305_SHA256,
+					tls.TLS_AES_256_GCM_SHA384,
                     tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
                     tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
                     tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
